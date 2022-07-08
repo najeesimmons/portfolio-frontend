228d8d5825b7ac52c21a609593278f738f0055dc
najeesimmons <najee.bryant.simmons@gmail.com>
2022-07-08 11:35:08 -0400

    refactored app.js with css modules

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ import Footer from "./components/Footer/Footer";
 // IMPORT PAGES
 import {About} from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import { AppWrapper } from "./App.styles";
+import classes from "./App.module.css"
 
 function App() {
   // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
   const URL = "https://najee-portfolio-backend.herokuapp.com/";
 
   return (
-    <div className="App">
+    <div className={classes.App}>
       <Nav />
-        <AppWrapper>
+        <div className={classes["app-wrapper"]}>
         {/* passing the URL variable above as props to the About and Project components */}
           <About url={URL}  />
           <Projects url={URL} />
-        </AppWrapper>
+        </div>
       <Footer />
     </div>
   );
--- src/App.styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-export const AppWrapper = styled.div`
-    max-width: 1440px;
-    margin: 0 auto;
-`;
