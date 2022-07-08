66d330d8e76b8b3360d557dd336528a206d6b811
najeesimmons <najee.bryant.simmons@gmail.com>
2022-07-08 12:48:37 -0400

    removed old code from Nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -21,26 +21,6 @@ function Nav() {
      </div>
     </header>
   );
-
-  // <header>
-  //    <NavBarWrapper>
-  //    <NavBar>
-  //       {/* <img src="https://i.imgur.com/v7h7Yxp.png" alt="initials" /> */}
-  //       <h3 color="white">Najee Simmons</h3>
-  //       <ul>
-  //         <li><a href="#about">About</a></li>
-  //         <li><a href="#projects">Projects</a></li>
-  //         {/* <li onClick={() => scrollToSection(projectsSection)}>Projects</li> */}
-  //         <li><a href="https://github.com/najeesimmons">GitHub</a></li>
-  //         <li><a href="https://drive.google.com/file/d/1kJ0ur7QcFvOZsgATNAL_JFJDWjNW2Ru0/view?usp=sharing">Resume</a></li>
-  //         <li>Contact Me</li>
-  //       </ul>
-  //     </NavBar>
-  //    </NavBarWrapper>
-  //   </header>
-  // );
 }
-
-
 
 export default Nav;
