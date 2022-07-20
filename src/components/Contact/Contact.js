c30a67cf78b267fe22511bbaaacfec9b8386c458
najeesimmons <najee.bryant.simmons@gmail.com>
2022-07-20 12:19:38 -0400

    media queries of entire app now ready for testing

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,18 @@
 import React from "react";
 import classes from "./Contact.module.css";
 
-
 const Contact = () => {
   return (
-    <div className={classes['contact-wrapper']}>
+    <div className={classes["contact-wrapper"]}>
       <div className={classes["contact-container"]}>
         <div className={classes["inner-container"]}>
-          <h1>Get In Touch<span role="img" alt="mailbox emoji" aria-label="mailbox emoji"> 📬</span></h1>
+          <h1>
+            Get In Touch
+            <span role="img" alt="mailbox emoji" aria-label="mailbox emoji">
+              {" "}
+              📬
+            </span>
+          </h1>
           <p className={classes.thanks}>
             Thanks for checking out my portfolio! I hope you'll reach out and
             say hello, whether you want to make me aware of a job opportunity or
@@ -18,12 +23,11 @@ const Contact = () => {
               href="mailto:najee.bryant.simmons@gmail.com?subject=Hello+World&body=Hello+World"
               className={classes["message-button"]}
             >
-              Get in Touch 
+              Get in Touch
             </a>
           </div>
         </div>
       </div>
-  
     </div>
   );
 };
